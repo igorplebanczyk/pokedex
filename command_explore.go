624e10799c96829362da1e57c5879de58ed8295d
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-07-20 13:50:20 +0200

    Fix lack of handling for no arguments in the explore command

--- command_explore.go
+++ command_explore.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func commandExplore(cfg *config, location string) error {
+	if location == "" {
+		return fmt.Errorf("location is required")
+	}
+
 	endpoint := "/location-area/" + location
 	resp, err := cfg.pokeapiClient.GetLocationAreaDetails(&endpoint)
 	if err != nil {
